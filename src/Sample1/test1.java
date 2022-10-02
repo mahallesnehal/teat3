fa39880f04555caec3ad5774b9bbbd6e0910a470
HP <HP@LAPTOP-1EE5KD20>
2022-10-02 17:39:27 +0530

    First commit.

---
+++ src/Sample1/test1.java
@@ -0,0 +1,15 @@
+package Sample1;
+
+public class test1 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("method running");
+		S1();
+	}
+	public static void S1()
+	{
+		System.out.println("hiii");
+	}
+
+}
