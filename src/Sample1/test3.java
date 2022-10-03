425ae147645695f209de9e9bb8fb6d8013ef6fef
HP <HP@LAPTOP-1EE5KD20>
2022-10-03 11:18:34 +0530

    first commit here.

---
+++ src/Sample1/test3.java
@@ -0,0 +1,20 @@
+package Sample1;
+
+import java.util.Scanner;
+
+public class test3 
+{
+	public static void main(String[] args)
+	{
+		Scanner scan=new Scanner(System.in);
+		
+		System.out.println("Enter first number");
+		           int S1 =scan.nextInt();
+		System.out.println("Enter second number"); 
+		             int S2 =scan.nextInt();
+		        System.out.println(S1+S2);
+		
+		
+	}
+
+}
---
+++ src/Sample1/test4.java
@@ -0,0 +1,20 @@
+package Sample1;
+
+public class test4
+{
+	public static void main(String[] args)
+	{
+		int a=20;
+		int b=30;
+		
+		    a=a+b;//50
+		    b=a-b;//20    50-30=20
+		    a=a-b;// 30     50-20=30
+		    System.out.println(b);
+		    System.out.println(a);
+		     
+		      
+		
+	}
+
+}
